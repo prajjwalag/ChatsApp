1a53fe4e718ad1edd4e1d198693bb37448b08b01
Prajjwal Agrahari <prajjwal.agrahari16@gmail.com>
2021-03-14 21:12:51 +0530

    Phone Authentication Done

--- app/src/main/java/com/prajjwal/chatsapp/PhoneNumberActivity.java
+++ app/src/main/java/com/prajjwal/chatsapp/PhoneNumberActivity.java
@@ -2,13 +2,33 @@ package com.prajjwal.chatsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.prajjwal.chatsapp.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        binding.phoneBox.requestFocus();
+
+        binding.continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this, OTPActivity.class);
+                intent.putExtra("phoneNumber", binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
