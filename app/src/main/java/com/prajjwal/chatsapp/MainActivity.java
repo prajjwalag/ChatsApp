61e43537fae3b94787a075663bd7915de31367b4
Prajjwal Agrahari <prajjwal.agrahari16@gmail.com>
2021-03-15 17:58:09 +0530

    Designed Main Activity and Setup User profile

--- app/src/main/java/com/prajjwal/chatsapp/MainActivity.java
+++ app/src/main/java/com/prajjwal/chatsapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.prajjwal.chatsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show();
+            case R.id.setting:
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/com/prajjwal/chatsapp/OTPActivity.java
+++ app/src/main/java/com/prajjwal/chatsapp/OTPActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -83,7 +84,9 @@ public class OTPActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()) {
-                            Toast.makeText(OTPActivity.this, "Logged In.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(OTPActivity.this, SetupProfileActivity.class);
+                            startActivity(intent);
+                            finishAffinity();
                         }
                         else {
                             Toast.makeText(OTPActivity.this, "Failed", Toast.LENGTH_SHORT).show();
